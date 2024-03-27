418dbf19357e8870d56929abf4f61234cc79fcd9
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-03-27 18:27:11 -0300

    Finalicé el ejercicio 3

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -1,1 +1,15 @@
 //3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+
+let cadAux = [];
+let cancelar;
+let cadena;
+do {
+  cadena = prompt("Ingrese una frase");
+  if (cadena !== null && cadena.trim() !== "") {
+    cadAux.push(cadena);
+  }
+  cancelar = confirm(
+    "Si desea continuar pulse Aceptar, caso contrario pulse Cancelar"
+  );
+} while (cancelar === true);
+alert(cadAux.join(" - "));
