3ea8aca65e46d11b2d44d25708ad5653773fe98d
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-03-30 11:06:30 -0300

    En el ejercicio 10 modifiqué el condicional de las validaciones y lo cambié por un bucle while así el ingreso de datos resulte más amigable para el usuario

--- ejercicio10/ejercicio10.js
+++ ejercicio10/ejercicio10.js
@@ -1,20 +1,22 @@
 //10- Realiza un script que pida número de filas y columnas y escriba una tabla. Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 
 let filas = prompt("Ingrese el numero de filas");
+while (isNaN(filas) || filas.trim() === "" || filas%1!==0) {
+  filas = prompt("Ingrese un numero válido");
+}
 let columnas = prompt("Ingrese el numero de columnas");
+while (isNaN(columnas) || columnas.trim() === "" || columnas%1!==0) {
+  columnas = prompt("Ingrese un numero válido");
+}
 
-if (isNaN(filas || isNaN(columnas))) {
-  alert("Ingrese un numero válido");
-} else {
-  filas = parseInt(filas);
-  columnas = parseInt(columnas);
-  let multiplicacion = filas * columnas;
-  for (let i = 0; i < filas; i++) {
-    for (let j = 0; j < columnas; j++) {
-      document.write(multiplicacion);
-      document.write(' ');
-      multiplicacion--;
-    }
-    document.write("<br>");
+filas = parseInt(filas);
+columnas = parseInt(columnas);
+let multiplicacion = filas * columnas;
+for (let i = 0; i < filas; i++) {
+  for (let j = 0; j < columnas; j++) {
+    document.write(multiplicacion);
+    document.write(" ");
+    multiplicacion--;
   }
+  document.write("<br>");
 }
