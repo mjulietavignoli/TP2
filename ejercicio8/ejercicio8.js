71b5ca73950b132301eff629f0f6dc3d52e47754
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-03-30 17:41:51 -0300

    Modifiqué las validaciones

--- ejercicio8/ejercicio8.js
+++ ejercicio8/ejercicio8.js
@@ -8,20 +8,23 @@
 123456
 ……*/
 
-let numero = prompt('Ingrese un numero del 1 al 50');
-if (isNaN(numero) || !Number.isInteger(Number(numero))) {
-  document.write("Ingrese un numero entero");
-} else {
-  numero = parseInt(numero);    
-  if (numero >= 1 && numero <= 50) {
-    for (let i = 1; i <= numero; i++) {
-      for (let j = 1; j <= i; j++) {
-        document.write(j);      
-      }
-      document.write("<br>");
+let numero;
+do {
+  numero = prompt("Ingrese un numero del 1 al 50");
+} while (
+  isNaN(numero) ||
+  numero % 1 !== 0 ||
+  numero === null ||
+  numero.trim() === ""
+);
+numero = parseInt(numero);
+if (numero >= 1 && numero <= 50) {
+  for (let i = 1; i <= numero; i++) {
+    for (let j = 1; j <= i; j++) {
+      document.write(j);
     }
-  } else{
-    alert('Ingrese un numero del 1 al 50');
+    document.write("<br>");
   }
+} else {
+  alert(`${numero} no se encuentra entre el 1 y el 50`);
 }
-
