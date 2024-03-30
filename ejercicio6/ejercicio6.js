2f05656c94197a2f7b244ecaf6e37da1d9e89351
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-03-30 17:24:32 -0300

    Modifiqué el ejercicio debido a un error en la lógica para validar si se presionaban los botones Aceptar o Cancelar

--- ejercicio6/ejercicio6.js
+++ ejercicio6/ejercicio6.js
@@ -8,17 +8,25 @@
 666666
 …….*/
 
-let numero = prompt("Ingrese un numero del 1 al 30");
-if (isNaN(numero) || !Number.isInteger(Number(numero))) {
-  document.write("Ingrese un numero entero");
-} else {
-  numero = parseInt(numero);    
+let numero;
+do {
+  numero = prompt("Ingrese un numero del 1 al 30");
+  if (numero === null || numero.trim() === "") {
+    alert("Debe ingresar un número");
+  }
+} while (numero === null || numero.trim() === "");
+if (!isNaN(numero) && numero !== "" && numero % 1 === 0) {
+  numero = parseInt(numero);
   if (numero >= 1 && numero <= 30) {
     for (let i = 1; i <= numero; i++) {
-        document.write("<br>");
+      document.write("<br>");
       for (let j = 1; j <= i; j++) {
-        document.write(i);      
+        document.write(i);
       }
     }
+  } else {
+    alert(`${numero} no se encuentra en el rango del 1 al 30`);
   }
+} else {
+  alert("Carga erronea, ingrese un numero entero del 1 al 30");
 }
