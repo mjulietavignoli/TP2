9e52704234465bc4a23d8593174e9e8c9c1b6141
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-03-30 16:56:55 -0300

    Cambié las validaciones debido a que tenían un problema en la lógica y las uncluí dentro de un bucle while, anteriormente estaban dentro de una estructura if

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,17 +1,14 @@
 //1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
-
 let edad;
 do {
-  edad = prompt('Ingrese su edad');
-  if(isNaN(edad) || edad==='' || edad===null || edad!==Number.isInteger(Number(edad))){ 
-    document.write('Ingrese un número entero');
-    break;
-  } else{
-    edad=parseInt(edad);
-    if (edad >= 18) {
-        alert('Puede conducir');
-      } else {
-        alert('Debe ser mayor de 18 años para conducir');
-      }
+  edad = prompt("Ingrese su edad");
+  while (isNaN(edad) || edad % 1 !== 0 || edad.trim() === '') {
+    edad = prompt("Carga erronea, ingrese nuevamente su edad");
   }
-} while (true);
+  edad = parseInt(edad);
+  if (edad >= 18) {
+    alert("Puede conducir");
+  } else {
+    alert("Debe ser mayor de 18 años para conducir");
+  }
+} while (edad!==null);
