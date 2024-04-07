9b9c979b7c7f6dabc7d8e6bc008c4b7aa0b5a0df
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-04-07 10:57:17 -0300

    Modifiqué el ejercicio en base a las correciones, eliminé la estructura do while y la cambié por una estructura while

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -31,27 +31,3 @@ while (nota !== null) {
   }
   nota = prompt("Ingrese la nota");
 }
-
-/*do {
-  let nota = prompt("Ingrese la nota");
-  if (nota < 0 || nota > 10) {
-    alert("Numero erróneo");
-  } else if (isNaN(nota)) {
-    alert("Introduce un numero válido");
-  } else {
-    nota = parseInt(nota);
-    if (nota >= 0 && nota <= 2) {
-      alert("Muy deficiente");
-    } else if (nota === 3 || nota === 4) {
-      alert("Insuficiente");
-    } else if (nota === 5 || nota ===6) {
-      alert("Suficiente");
-    } else if (nota === 7) {
-      alert("Bien");
-    } else if (nota === 8 || nota === 9) {
-      alert("Notable");
-    } else if (nota === 10) {
-      alert("Sobresaliente");
-    }
-  }
-} while (true);*/
