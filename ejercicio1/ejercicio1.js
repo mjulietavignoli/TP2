73fe74434c1764c81475b4d3f4f9b8fa4e6cdf46
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-04-07 10:50:46 -0300

    Modifiqué el ejercicio en base a las correciones, eliminé la estructura do while y la cambié por una estructura while

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,14 +1,17 @@
 //1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
 let edad;
-do {
-  edad = prompt("Ingrese su edad");
-  while (isNaN(edad) || edad % 1 !== 0 || edad.trim() === '') {
+let correcto = false;
+edad = prompt("Ingrese su edad");
+while (edad !== null) {
+  while (isNaN(edad) || edad % 1 !== 0 || edad === "") {
     edad = prompt("Carga erronea, ingrese nuevamente su edad");
   }
   edad = parseInt(edad);
   if (edad >= 18) {
     alert("Puede conducir");
+    correcto = true;
   } else {
     alert("Debe ser mayor de 18 años para conducir");
   }
-} while (edad!==null);
+  edad = prompt("Ingrese su edad");
+}
