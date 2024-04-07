e691c569b3e47391ed9c0d9597bc806fae2ada3c
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-04-07 10:56:07 -0300

    Modifiqué el ejercicio en base a las correciones, eliminé la estructura do while y la cambié por una estructura while

--- ejercicio2/ejercicio2.js
+++ ejercicio2/ejercicio2.js
@@ -7,7 +7,32 @@
 10: Sobresaliente
 Si ingreso un número que no esté dentro del rango de 0 a 10 mostrar un mensaje de “número erróneo”. Si el número ingresado no es válido mostrar el mensaje “Introduce un número válido”.*/
 
-do {
+let nota = prompt("Ingrese la nota");
+while (nota !== null) {
+  if (nota < 0 || nota > 10) {
+    alert("Numero erróneo");
+  } else if (isNaN(nota)) {
+    alert("Introduce un numero válido");
+  } else {
+    nota = parseInt(nota);
+    if (nota >= 0 && nota <= 2) {
+      alert("Muy deficiente");
+    } else if (nota === 3 || nota === 4) {
+      alert("Insuficiente");
+    } else if (nota === 5 || nota === 6) {
+      alert("Suficiente");
+    } else if (nota === 7) {
+      alert("Bien");
+    } else if (nota === 8 || nota === 9) {
+      alert("Notable");
+    } else if (nota === 10) {
+      alert("Sobresaliente");
+    }
+  }
+  nota = prompt("Ingrese la nota");
+}
+
+/*do {
   let nota = prompt("Ingrese la nota");
   if (nota < 0 || nota > 10) {
     alert("Numero erróneo");
@@ -29,4 +54,4 @@ do {
       alert("Sobresaliente");
     }
   }
-} while (true);
+} while (true);*/
